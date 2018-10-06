aeef0895f56a6a7bd782595a4af1a058a25467fd
shivumgrover <shivumgrover99@gmail.com>
2018-10-06 03:02:45 -0700

    many changes

--- app/src/main/java/com/example/fgw/ighackuserr/barcodein2.java
+++ app/src/main/java/com/example/fgw/ighackuserr/barcodein2.java
@@ -1,18 +1,35 @@
 package com.example.fgw.ighackuserr;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class barcodein2 extends AppCompatActivity {
+    LottieAnimationView animationView;
+    private FirebaseAuth mAuth;
+    public String keyl;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,15 @@ public class barcodein2 extends AppCompatActivity {
                 intentIntegrator.setBeepEnabled(false);
                 intentIntegrator.setBarcodeImageEnabled(false);
                 intentIntegrator.initiateScan();
+        animationView = (LottieAnimationView) findViewById(R.id.animation_view2);
+        animationView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animationView.playAnimation();
+                requestLocationUpdates();
+
+            }
+        });
 
     }
     @Override
@@ -49,6 +75,44 @@ public class barcodein2 extends AppCompatActivity {
         }
         return super.onKeyLongPress(keyCode, event);
     }
+
+    private void requestLocationUpdates() {
+
+        LocationRequest request = new LocationRequest();
+        request.setInterval(1000);
+        request.setFastestInterval(50);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+//        final String path = getString(R.string.firebase_path) + "/" + keyl;
+        final String path = "location" + "/" + keyl;
+
+//        Toast.makeText(getBaseContext(),keyl,Toast.LENGTH_SHORT).show();
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            // Request location updates and when an update is
+            // received, store the location in Firebase
+            client.requestLocationUpdates(request, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("userLocation");
+                    Location location = locationResult.getLastLocation();
+//                    latitudecurrent = location.getLatitude();
+//                    longitudecurrent = location.getLongitude();
+                    if (location != null) {
+//                        Log.d(TAG, "location update " + location);
+                        ref.setValue(location);
+                    }
+//                    longitudes.setText( location.getLongitude()+"");
+//                    la.setText( location.getLatitude()+"");
+                }
+            }, null);
+        }
+
+    }
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
